7a0d31638b6450ee69190baba6a089948a1bfa62
Yuning Xia <yx87@rice.edu>
2024-08-30 19:38:46 +0000

    add multi-dev-multi-thread

--- multi-dev-multi-thread/common.h
+++ multi-dev-multi-thread/common.h
@@ -9,7 +9,8 @@ constexpr size_t N_LOOP = 40000000;
 constexpr size_t N_SINGLE = 40000000;
 constexpr size_t LOOP_COUNT = 10;
 
-cl::sycl::queue initgpu();
+cl::sycl::queue initgpu(int deviceIndex = -1);
+
 void vecadd_kernel(cl::sycl::queue &queue, std::vector<int> &a, std::vector<int> &b, std::vector<int> &c, size_t N, int thread_id, int iteration);
 void kernel_submission_1(cl::sycl::queue queue, int thread_id, size_t X);
 void kernel_submission_2(cl::sycl::queue queue, int thread_id);
