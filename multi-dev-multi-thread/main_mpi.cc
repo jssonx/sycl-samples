0eeca6ae089d6ca7e2e1150c8ee333b39605405f
Yuning Xia <yx87@rice.edu>
2024-09-13 19:13:26 +0000

    update mpi

--- multi-dev-multi-thread/main_mpi.cc
+++ multi-dev-multi-thread/main_mpi.cc
@@ -10,32 +10,27 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    std::vector<cl::sycl::device> devices;
-
     try
     {
-        if (rank == 0)
-        {
-            devices = initgpu();
-            int num_devices = devices.size();
-            MPI_Bcast(&num_devices, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        }
-        else
-        {
-            int num_devices;
-            MPI_Bcast(&num_devices, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            devices.resize(num_devices);
-        }
+        // Each rank queries its available devices
+        std::vector<cl::sycl::device> devices = cl::sycl::device::get_devices(cl::sycl::info::device_type::gpu);
 
-        if (rank >= devices.size()) {
-            std::cerr << "Error: Not enough devices for rank " << rank << std::endl;
+        if (devices.empty()) {
+            std::cerr << "Error: No devices found for rank " << rank << std::endl;
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
 
-        cl::sycl::queue queue = createQueue(devices[rank]);
+        // Each rank will work with the first available device (as limited by ZE_AFFINITY_MASK)
+        cl::sycl::queue queue = createQueue(devices[0]);
+
+        std::cout << "Rank " << rank << " is using device: " 
+                  << queue.get_device().get_info<cl::sycl::info::device::name>() << std::endl;
+
+        // Perform kernel execution on the rank-specific device
         kernel_submission(queue, 10000000, "kernel" + std::to_string(rank));
 
         MPI_Barrier(MPI_COMM_WORLD);
+
         if (rank == 0) {
             std::cout << "All ranks have finished execution.\n";
         }
